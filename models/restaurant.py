7b62c1f7e60833646f350c2f1f807a931376f840
Gabriel Oliveira de Lima Rodrigues <g.de.lima.rodrigues@avanade.com>
2024-10-16 12:12:56 -0300

    feat: Adicionando classe Restaurante

---
+++ models/restaurant.py
@@ -0,0 +1,26 @@
+class Restaurant:
+    restaurants = []
+    
+    def __init__(self, name, category):
+        self._name = name.title()
+        self.category = category.upper()
+        self._active = False
+        Restaurant.restaurants.append(self)
+        
+    @classmethod
+    def show_restaurants(cls):
+        header = f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}'
+        print(header)
+        print('-' * len(header))
+        for restaurant in cls.restaurants:                
+            print(f'{restaurant._name.ljust(25)} | {restaurant.category.ljust(25)} | {restaurant.active}')
+            
+    @property
+    def active(self):
+        return '☑' if self._active else '☐'
+    
+restaurant_praca = Restaurant('Praça São Lourenço', 'Brasileira')
+restaurant_madero = Restaurant('Madero', 'Hamburgueria')
+restaurant_montana = Restaurant('Montana', 'Churrascaria')
+
+Restaurant.show_restaurants()
